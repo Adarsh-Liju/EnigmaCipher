031471d7521a890abe378fc9f62b3d6618dc05d2
Adarsh Liju Abraham <adarsh.liju.abraham@gmail.com>
2022-10-11 08:46:03 +0530

    Add CMake Build System
    Signed-off-by: Adarsh Liju Abraham <adarsh.liju.abraham@gmail.com>

---
+++ CMakeFiles/EnigmaCipher.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EnigmaCipher.dir/main.c.o"
+  "CMakeFiles/EnigmaCipher.dir/main.c.o.d"
+  "EnigmaCipher"
+  "EnigmaCipher.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/EnigmaCipher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
